584128a0e29b75b42ffbcaf18d84fdbfc77e785f
João Pedro Barboza Rigoto Ferreira <joaopedro.rigoto@gmail.com>
2026-02-28 20:10:55 -0300

    Exercício prático iniciado

---
+++ C# e .NET/Aula1.2/Ex_Pratico1.cs
@@ -0,0 +1,19 @@
+﻿namespace C__e_.NET.Aula1._2
+{
+    internal class Ex_Pratico1
+    {
+    /*
+    Implemente um programa de caixa registradora para uma loja (tema do aluno)
+    O programa deve calcular o total de um pedido apenas uma vez por execução do programa
+    Imprima duas opções de de produto da loja (ex: 1 - beef burguer e 2 - fish burguer)
+    Considere que o produto 1 e 2 tenham valor fixo de R$ 50,00 e R$ 60,00 respectivamente
+    Leia a opção de produto desejado pelo usuário
+    Leia a quantidade desejada, calcule e imprima o total do pedido    
+    --LOJA DE CARROS
+    */
+        static void Main(string[] args)
+        {
+             
+        }
+    }
+}
